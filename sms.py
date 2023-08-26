6678c51f916942dcbf7f37ffc4afe3e2b8f76233
charles <gitauwairimu@gmail.com>
2023-08-26 10:30:02 +0300

    removed unnecesary code

--- sms.py
+++ sms.py
@@ -7,18 +7,10 @@ AUTH_TOKEN = env_vars[4].split("=")[1]
 def send_sms(to, body):
   from twilio.rest import Client
 
-  # account_sid = 'AC6b96af4ceebfef859e74bd30f48de009'
-  # auth_token = '83c48141f5dfb7cbbe1d74898e5c7620'
-  
   account_sid = ACCOUNT_SID
   auth_token = AUTH_TOKEN
-  print(account_sid)
-  print(auth_token)
-  client = Client(account_sid, auth_token)
 
-  # admin_no = 1234
-  # body = f"Your admin_no is {admin_no}.")
-  # body = f"Your admin_no is {admin_no}"
+  client = Client(account_sid, auth_token)
 
   message = client.messages.create(
     from_='+14706885125',
@@ -26,11 +18,5 @@ def send_sms(to, body):
     to=to
   )
 
-
-  # message = client.messages.create(
-  #   from_='+14706885125',
-  #   body=body,
-  #   to='+254720051528'
-  # )
 
   print(message.sid)
