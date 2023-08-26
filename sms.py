1f1dc32c9f096491f7f31d8b6c7e1275e881e9f4
charles <gitauwairimu@gmail.com>
2023-08-26 08:44:15 +0300

    parameterized confidential information

--- sms.py
+++ sms.py
@@ -1,46 +1,35 @@
-# def send_sms(to, from_, body):
-#   # Import the Twilio API.
-#   import twilio
-#   from twilio.rest import Client
+with open(".env") as f:
+  env_vars = f.read().splitlines()
 
-#   # Your Twilio account SID and auth token.
-#   account_sid = "AC6b96af4ceebfef859e74bd30f48de009"
-#   auth_token = "db12e28e521c61de1d526f91478d6252"
+ACCOUNT_SID = env_vars[3].split("=")[1]
+AUTH_TOKEN = env_vars[4].split("=")[1]
 
-#   # Create a Twilio client.
-#   client = Client(account_sid, auth_token)
+def send_sms(to, body):
+  from twilio.rest import Client
 
-#   # Send an SMS message.
-#   message = client.messages.create(
-#     to=to,
-#     from_=from_,
-#     body=body)
+  # account_sid = 'AC6b96af4ceebfef859e74bd30f48de009'
+  # auth_token = '83c48141f5dfb7cbbe1d74898e5c7620'
 
-#   print(message.sid)
+  account_sid = ACCOUNT_SID
+  auth_token = AUTH_TOKEN
 
-# # Get the admin_no.
-# admin_no = "123456"
+  client = Client(account_sid, auth_token)
 
-# # Send the SMS message.
-# send_sms(to="+254720051528", from_="+14706885125", body=f"Your admin_no is {admin_no}.")
+  # admin_no = 1234
+  # body = f"Your admin_no is {admin_no}.")
+  # body = f"Your admin_no is {admin_no}"
 
-
-# Download the helper library from https://www.twilio.com/docs/python/install
-from twilio.rest import Client
-
-account_sid = 'AC6b96af4ceebfef859e74bd30f48de009'
-auth_token = '83c48141f5dfb7cbbe1d74898e5c7620'
-client = Client(account_sid, auth_token)
-
-admin_no = 1234
-# body = f"Your admin_no is {admin_no}.")
-body = f"Your admin_no is {admin_no}"
+  message = client.messages.create(
+    from_='+14706885125',
+    body=body,
+    to=to
+  )
 
 
-message = client.messages.create(
-  from_='+14706885125',
-  body=body,
-  to='+254720051528'
-)
+  # message = client.messages.create(
+  #   from_='+14706885125',
+  #   body=body,
+  #   to='+254720051528'
+  # )
 
-print(message.sid)
+  print(message.sid)
