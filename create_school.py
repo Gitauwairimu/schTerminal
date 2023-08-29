6a2f18c2abaa27a36b9deee6fa610b413b496135
charles <gitauwairimu@gmail.com>
2023-08-29 11:55:03 +0300

    added create school, classes and streams functionality

--- create_school.py
+++ create_school.py
@@ -1,16 +1,27 @@
-def create_school():
-  """Creates a school and returns its name."""
+def create_classes():
+  """Creates a class and returns its name."""
 
   # Ask the user for the name of the school.
-  school_name = input("Enter the name of the school: ")
+  class_name = input("Enter the name of the class: ")
+  streams_number = int(input("How many streams does class have?: "))
+  max_learners = input("Enter the maximum learners class accomodates: ")
+
+  streams = 0
+  stream_names = []
+  while streams_number > streams:
+    stream_name = class_name + '_stream_' + str(streams + 1)
+    streams +=1
+    stream_names.append(stream_name)
+  print(stream_names)
 
   # Return the school name.
-  return school_name
+  return class_name
 
 
 if __name__ == "__main__":
-  # Create the school.
+  # # Create the school.
   school_name = create_school()
 
-  # Print the name of the school.
-  print(f"School Name: {school_name}.")
+  # # Print the name of the school.
+  # print(f"School Name: {school_name}.")
+  create_classes()
