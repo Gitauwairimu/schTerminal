23707ebd2029e1dec21620b1ffc5b60db1c48bf7
charles <gitauwairimu@gmail.com>
2023-09-08 04:13:51 +0300

    run bash script and also scp playbook

---
+++ ansible/prep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update the server
+sudo apt update && sudo apt upgrade -y
+
+# Install Ansible
+sudo apt install ansible -y
+
+# Run the Ansible playbook
+ansible-playbook playbook.yml
