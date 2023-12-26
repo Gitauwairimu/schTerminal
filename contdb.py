3f6775952d464c100ef11076e7fd285bbf6537aa
charles <gitauwairimu@gmail.com>
2023-12-26 16:57:46 +0300

    rm port as part of connection string dict

--- contdb.py
+++ contdb.py
@@ -36,14 +36,23 @@ def connect_to_database():
 
   connection_string = {
     "host": "os.getenv('DB_HOST')",
-    "port": "os.getenv('DB_PORT')",
     "database": "os.getenv('DB_DATABASE')",
     "user": "os.getenv('DB_USER')",
     "password": "os.getenv('DB_PASSWORD')"
   }
+
+  # connection_string = {
+  #   "host": "os.getenv('DB_HOST')",
+  #   "port": "os.getenv('DB_PORT')",
+  #   "database": "os.getenv('DB_DATABASE')",
+  #   "user": "os.getenv('DB_USER')",
+  #   "password": "os.getenv('DB_PASSWORD')"
+  # }
+
+  port='5432'
  
   # Construct the connection string as a string
-  connection_string = f"host={connection_string['host']} port=int({connection_string['port']}) dbname={connection_string['database']} user={connection_string['user']} password={connection_string['password']}"
+  connection_string = f"host={connection_string['host']} port=port dbname={connection_string['database']} user={connection_string['user']} password={connection_string['password']}"
 
 
   # Connect to the database.
